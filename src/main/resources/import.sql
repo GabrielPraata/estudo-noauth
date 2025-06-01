3574348e7a414ad94ee54e946286917109ecba72
Gabriel Prata <ghprata7245@gmail.com>
2025-06-01 11:57:56 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$kfkD.UqOPhS39/DQPqLb1.gboGr6AR3iIcIbJ7xKbYm4P9v7n3gEi');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$kfkD.UqOPhS39/DQPqLb1.gboGr6AR3iIcIbJ7xKbYm4P9v7n3gEi');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
